7c622ba96e22f9b525445d4afb01641dae28313f
Devank Singh <adreshsingh271527@gmail.com>
2025-02-19 07:30:59 +0530

    project entery point bug

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/blog-card-arpit/",
+  base: "/social-link-card/",
   plugins: [
     tailwindcss(),
     react()
